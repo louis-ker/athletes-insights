74e8e72b06aab3877076ae2db360c9fd1c5e7809
louis-ker <louiskerguillec35@gmail.com>
2025-10-28 10:28:33 +0100

    [MOD] port binding

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,6 @@ def hello():
     return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
